644b877e2a3e0082b1850b35093b94f068ae1672
Daniel <danieliozzi@gmail.com>
2016-06-22 12:00:43 +0200

    serialize to file , default async

--- src/encoder.ts
+++ src/encoder.ts
@@ -1,42 +1,42 @@
 import * as fs from 'fs';
 
-export interface iDisctionary<T extends string|number|symbol> {
+export interface iDisctionary<T extends string | number | symbol> {
     [index: string]: T;
 }
 
-export function isMap(x:any) : x is Map<any,any>{
+export function isMap(x: any): x is Map<any, any> {
     return x instanceof Map
 }
 
-export function toObject(map: Map<any,any>): Object {
+export function toObject(map: Map<any, any>): Object {
 
-    var o :any = Object.create(null);
+    var o: any = Object.create(null);
 
-    for(let [k, v] of map.entries()) {
-        o[k] = isMap(v) ? toObject(v): v;
+    for (let [k, v] of map.entries()) {
+        o[k] = isMap(v) ? toObject(v) : v;
     }
     return o;
 }
 
-export function isObject(x:any){
+export function isObject(x: any) {
     return typeof x == 'object';
 }
 
-export function toMap<TKey>(obj:iDisctionary<any>) : Map<TKey,any>{
-    let strMap = new Map<TKey,any>();
+export function toMap<TKey>(obj: iDisctionary<any>): Map<TKey, any> {
+    let strMap = new Map<TKey, any>();
     for (let k of Object.keys(obj)) {
         var value = obj[k];
-        strMap.set(k as any, isObject(value) ? toMap(value) : value );
+        strMap.set(k as any, isObject(value) ? toMap(value) : value);
     }
     return strMap;
 }
 
-export function serializeMapSync<K,V>(map : Map<K,V>) : string {
+export function serializeMapSync<K, V>(map: Map<K, V>): string {
     return JSON.stringify(toObject(map));
 }
 
-function serializeMapAsync<TKey,TValue>(map:Map<TKey,TValue>):Promise<string> {
-    return new Promise((resolve, reject)=> {
+function serializeMapAsync<TKey, TValue>(map: Map<TKey, TValue>): Promise<string> {
+    return new Promise((resolve, reject) => {
         try {
             resolve(serializeMapSync(map))
         } catch (e) {
@@ -46,55 +46,69 @@ function serializeMapAsync<TKey,TValue>(map:Map<TKey,TValue>):Promise<string> {
 }
 
 
-export function serializeToFile<K,V>(filePath: string, map : Map<K,V>) : void {
-     fs.writeFileSync(
+export function serializeToFileSync<K, V>(filePath: string, map: Map<K, V>): void {
+    fs.writeFileSync(
         filePath,
         JSON.stringify(toObject(map))
     );
 }
 
-export function deserialize<K,V>(json :string) :  Map<K,V> {
+export function serializeToFile<K, V>(filePath: string, map: Map<K, V>): Promise<any> {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(
+            filePath,
+            JSON.stringify(toObject(map)),
+             e=> {
+                 if(e) {reject(e) ; return ;}
+                 resolve(true);
+             }
+        );
+    })
+
+}
+
+export function deserialize<K, V>(json: string): Map<K, V> {
     return toMap<K>(JSON.parse(json));
 }
 
-export function deserializeFromFileSync<K,V>(filePath:string) :  Map<K,V> {
+export function deserializeFromFileSync<K, V>(filePath: string): Map<K, V> {
     toMap(JSON.parse(fs.readFileSync(filePath, 'utf-8')));
     return
 }
 
-export function deserializeFromFile<K,V>(filePath:string) :  Promise<Map<K,V>> {
-        
-    return new Promise((rs,rj)=>{
-            fs.readFile(filePath, 'utf-8', (err, data)=>{
-                if(err){
-                    rj(err);
-                }
-                var x = toMap(JSON.parse(data));
-                rs(x);
-            });
-        })
+export function deserializeFromFile<K, V>(filePath: string): Promise<Map<K, V>> {
+
+    return new Promise((rs, rj) => {
+        fs.readFile(filePath, 'utf-8', (err, data) => {
+            if (err) {
+                rj(err);
+            }
+            var x = toMap(JSON.parse(data));
+            rs(x);
+        });
+    })
 }
 
 
 
-export function toMaps<T,TKey>(key: (target:T) => TKey, targets:T[]) : Map<TKey,Map<string, any>> {
+export function toMaps<T, TKey>(key: (target: T) => TKey, targets: T[]): Map<TKey, Map<string, any>> {
 
-    var map = new Map<TKey,Map<string,any>>();
+    var map = new Map<TKey, Map<string, any>>();
 
-    targets.forEach(target=> {
+    targets.forEach(target => {
 
         map.set(key(target), toMap<string>(target));
     });
 
-    return map ;
+    return map;
 }
 
-export function fromMap<T,TKey>(type: { new(): T ;} , map: Map<TKey, any>){
+export function fromMap<T, TKey>(type: { new (): T; }, map: Map<TKey, any>) {
 
-    var target  = new type();
+    var target = new type();
 
     // value: V, index: K, map: Map<K, V>
-    map.forEach(( v : any , k: TKey )=> {
+    map.forEach((v: any, k: TKey) => {
         (target as any)[k as any] = v
     });
 
@@ -103,11 +117,11 @@ export function fromMap<T,TKey>(type: { new(): T ;} , map: Map<TKey, any>){
 }
 
 
-export function fromMaps<T,TKey>(type: { new(): T ;} , maps:IterableIterator<Map<TKey,any>>) : T[] {
+export function fromMaps<T, TKey>(type: { new (): T; }, maps: IterableIterator<Map<TKey, any>>): T[] {
 
-    var result :T[] = [] ;
+    var result: T[] = [];
 
-    for(var map of maps){
+    for (var map of maps) {
 
         result.push(fromMap(type, map))
     }
